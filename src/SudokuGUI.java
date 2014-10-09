6fcefe7a20a62ff21b6ddfcb37a43848515f289e
tannermon11 <tannermon11@gmail.com>
2014-10-09 16:21:53 -0700

    Timer development

--- src/SudokuGUI.java
+++ src/SudokuGUI.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
 
 /**
@@ -8,15 +9,17 @@ import java.awt.*;
  * Time: 7:40 PM
  * To change this template use File | Settings | File Templates.
  */
-public class SudokuGUI extends JFrame{
+public class SudokuGUI extends JFrame {
     JFrame frame;
     JPanel panel, panel2, panel3, panel4, panel5, panel6, panel7, panel8, panel9, bPanel, tPanel;
     JButton play, load, login, register, highscore, manual, hints, pause, end;
-    JLabel user, timeLabel;
+    JLabel user, time;
     JTextField[][] textFields;
     JComboBox difficultyBox;
+    Timer timer = null;
     int rating, timeBonus, Accuracy, solveAids;
     int score;// = rating+timeBonus-Accuracy-solveAids;
+    int minutes_elapsed = 0, seconds_elapsed = 0;
 
     public SudokuGUI() {
         frame = new JFrame("Sudoku");
@@ -34,7 +37,8 @@ public class SudokuGUI extends JFrame{
         hints = new JButton("Hints");
         pause = new JButton("Pause");
         end = new JButton("End");
-        timeLabel = new JLabel("Time: ");
+        time = new JLabel();
+        time.setForeground(Color.blue);
         textFields = new JTextField[Sudoku.GRID_SIZE][Sudoku.GRID_SIZE];
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new GridLayout(4, 3));
@@ -48,9 +52,10 @@ public class SudokuGUI extends JFrame{
         panel7.setLayout(new GridLayout(3, 3));
         panel8.setLayout(new GridLayout(3, 3));
         panel9.setLayout(new GridLayout(3, 3));
-        bPanel.setLayout(new GridLayout(8,1));
+        bPanel.setLayout(new GridLayout(8, 1));
         tPanel.setLayout(new BorderLayout());
-        tPanel.add(timeLabel, BorderLayout.NORTH);
+
+        tPanel.add(time, BorderLayout.NORTH);
         frame.add(panel);
         frame.add(panel2);
         frame.add(panel3);
@@ -68,43 +73,66 @@ public class SudokuGUI extends JFrame{
             for (int y = 0; y < Sudoku.GRID_SIZE; y++) {
                 String position = Integer.toString(x) + "," + Integer.toString(y);
                 textFields[x][y] = new JTextField(position, 2);
-                if(x<3 && y<3)
+                if (x < 3 && y < 3)
                     panel.add(textFields[x][y]);
-                if(x<3 && y>=3 && y<6)
+                if (x < 3 && y >= 3 && y < 6)
                     panel2.add(textFields[x][y]);
-                if(x<3 && y>=6)
+                if (x < 3 && y >= 6)
                     panel3.add(textFields[x][y]);
 
-                if(x>=3 && x<6 && y<3)
+                if (x >= 3 && x < 6 && y < 3)
                     panel4.add(textFields[x][y]);
-                if(x>=3 && x<6 && y>=3 && y<6)
+                if (x >= 3 && x < 6 && y >= 3 && y < 6)
                     panel5.add(textFields[x][y]);
-                if(x>=3 && x<6 && y>=6)
+                if (x >= 3 && x < 6 && y >= 6)
                     panel6.add(textFields[x][y]);
 
-                if(x>=6 && y<3)
+                if (x >= 6 && y < 3)
                     panel7.add(textFields[x][y]);
-                if(x>=6 && y>=3 && y<6)
+                if (x >= 6 && y >= 3 && y < 6)
                     panel8.add(textFields[x][y]);
-                if(x>=6 && y>=6)
+                if (x >= 6 && y >= 6)
                     panel9.add(textFields[x][y]);
+
+
+                InputVerifier fieldVerifier = new InputVerifier() {
+                    @Override
+                    public boolean verify(JComponent input) {
+                        JTextField temp = (JTextField)input;
+                        try {
+                            int number = Integer.parseInt(temp.getText());
+                            if(temp.getText().trim().length() > 1)
+                                JOptionPane.showMessageDialog(null, "Only 1 number is allowed");
+                            return (temp.getText().trim().length() == 1);
+                        }
+                        catch (NumberFormatException e) {
+                            JOptionPane.showMessageDialog(null, "Only numbers are allowed");
+                        }
+                        return false;
+                    }
+                };
+            textFields[x][y].setInputVerifier(fieldVerifier);
 
             }
         }
         menu(); //Just seeing what it looks like currently - will move later
 
-        panel.setBorder(BorderFactory.createMatteBorder(2,2,2,2,Color.black));
-        panel2.setBorder(BorderFactory.createMatteBorder(2,2,2,2,Color.black));
-        panel3.setBorder(BorderFactory.createMatteBorder(2,2,2,2,Color.black));
-        panel4.setBorder(BorderFactory.createMatteBorder(2,2,2,2,Color.black));
-        panel5.setBorder(BorderFactory.createMatteBorder(2,2,2,2,Color.black));
-        panel6.setBorder(BorderFactory.createMatteBorder(2,2,2,2,Color.black));
-        panel7.setBorder(BorderFactory.createMatteBorder(2,2,2,2,Color.black));
-        panel8.setBorder(BorderFactory.createMatteBorder(2,2,2,2,Color.black));
-        panel9.setBorder(BorderFactory.createMatteBorder(2,2,2,2,Color.black));
+        panel.setBorder(BorderFactory.createMatteBorder(2, 2, 2, 2, Color.black));
+        panel2.setBorder(BorderFactory.createMatteBorder(2, 2, 2, 2, Color.black));
+        panel3.setBorder(BorderFactory.createMatteBorder(2, 2, 2, 2, Color.black));
+        panel4.setBorder(BorderFactory.createMatteBorder(2, 2, 2, 2, Color.black));
+        panel5.setBorder(BorderFactory.createMatteBorder(2, 2, 2, 2, Color.black));
+        panel6.setBorder(BorderFactory.createMatteBorder(2, 2, 2, 2, Color.black));
+        panel7.setBorder(BorderFactory.createMatteBorder(2, 2, 2, 2, Color.black));
+        panel8.setBorder(BorderFactory.createMatteBorder(2, 2, 2, 2, Color.black));
+        panel9.setBorder(BorderFactory.createMatteBorder(2, 2, 2, 2, Color.black));
 
         frame.setVisible(true);
         frame.setSize(500, 500);
+    }
+
+    private boolean legalNumber(int n) {
+        return n >= 1 && n <= 9;
     }
 
     public void menu() {
@@ -118,9 +146,17 @@ public class SudokuGUI extends JFrame{
         register = new JButton("Register");
         highscore = new JButton("Hall of Fame");
         manual = new JButton("How-To-Play");
+
+        play.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                startTimer(0,0);
+            }
+        });
+
         login.setBackground(Color.ORANGE);
         register.setBackground(Color.green);
-        highscore.setBackground(new Color(126,237,235));
+        highscore.setBackground(new Color(126, 237, 235));
         bPanel.add(user);
         bPanel.add(difficultyBox);
         bPanel.add(play);
@@ -129,9 +165,101 @@ public class SudokuGUI extends JFrame{
         bPanel.add(register);
         bPanel.add(highscore);
         bPanel.add(manual);
+
+        class tannerKeyListener extends KeyAdapter {
+
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_0)
+                    System.out.println("0 pressed");
+                displayInfo(e, "KEY TYPED: ");
+            }
+
+            public void keyReleased(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_0)
+                    System.out.println("0 pressed");
+                displayInfo(e, "KEY PRESSED: ");
+            }
+
+            public void keyTyped(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_0)
+                    System.out.println("0 pressed");
+                displayInfo(e, "KEY RELEASED: ");
+            }
+
+            private void displayInfo(KeyEvent e, String keyStatus) {
+
+                //You should only rely on the key char if the event
+                //is a key typed event.
+                int id = e.getID();
+                String keyString;
+                if (id == KeyEvent.KEY_TYPED) {
+                    char c = e.getKeyChar();
+                    keyString = "key character = '" + c + "'";
+                } else {
+                    int keyCode = e.getKeyCode();
+                    keyString = "key code = " + keyCode
+                            + " ("
+                            + KeyEvent.getKeyText(keyCode)
+                            + ")";
+                }
+
+                int modifiersEx = e.getModifiersEx();
+                String modString = "extended modifiers = " + modifiersEx;
+                String tmpString = KeyEvent.getModifiersExText(modifiersEx);
+                if (tmpString.length() > 0) {
+                    modString += " (" + tmpString + ")";
+                } else {
+                    modString += " (no extended modifiers)";
+                }
+
+                String actionString = "action key? ";
+                if (e.isActionKey()) {
+                    actionString += "YES";
+                } else {
+                    actionString += "NO";
+                }
+
+                String locationString = "key location: ";
+                int location = e.getKeyLocation();
+                if (location == KeyEvent.KEY_LOCATION_STANDARD) {
+                    locationString += "standard";
+                } else if (location == KeyEvent.KEY_LOCATION_LEFT) {
+                    locationString += "left";
+                } else if (location == KeyEvent.KEY_LOCATION_RIGHT) {
+                    locationString += "right";
+                } else if (location == KeyEvent.KEY_LOCATION_NUMPAD) {
+                    locationString += "numpad";
+                } else { // (location == KeyEvent.KEY_LOCATION_UNKNOWN)
+                    locationString += "unknown";
+                }
+            }
+
+        }
+
     }
 
-    private boolean legalNumber(int n) {
-        return n >= 1 && n <= 9;
+    public void startTimer(int minutes, int seconds) {
+        if(timer == null) {
+            timer = new Timer(0, new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    time.setText("Time: " + String.format("%02d:%02d", minutes_elapsed, seconds_elapsed));
+                    seconds_elapsed++;
+                    if(seconds_elapsed == 60) {
+                        seconds_elapsed = 0;
+                        minutes_elapsed++;
+                    }
+                }
+            });
+            timer.setDelay(1000);
+            timer.setRepeats(true);
+        }
+        timer.start();
     }
+
+    public void stopTimer() {
+        if(timer != null)
+            timer.stop();
+    }
+
 }
