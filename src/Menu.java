b772fc5ba3b933b3e336fe14414020c6d711b632
surfusa <surfusa@users.noreply.github.com>
2014-11-07 17:00:56 -0700

    Manual updated

--- src/Menu.java
+++ src/Menu.java
@@ -2,7 +2,10 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 /**
  * User: Tanner
  * Date: 10/30/2014
@@ -11,13 +14,17 @@ import java.awt.event.ActionListener;
  */
 
 public class Menu extends JFrame {
-    JPanel bPanel;
-    static JFrame frame;
-    JButton play, load, highscore, manual;
+    JPanel bPanel, mPanel;
+    static JFrame frame, mFrame;
+    JButton play, load, highscore, manual, back;
     JLabel user;
     static JComboBox difficultyBox;
     Timer timer = null;
     int minutes_elapsed = 0, seconds_elapsed = 0;
+    JTextArea mTextArea;
+    File manualFile = new File("Manual.txt");
+    FileReader fr = null;
+    BufferedReader reader = null;
 
     public Menu() {
         frame = new JFrame();
@@ -32,24 +39,72 @@ public class Menu extends JFrame {
         highscore = new JButton("Hall of Fame");
         manual = new JButton("How-To-Play");
 
-        difficultyBox.addActionListener(new ActionListener() {
+        difficultyBox.addActionListener(new ActionListener()
+        {
             @Override
-            public void actionPerformed(ActionEvent e) {
+            public void actionPerformed(ActionEvent e)
+            {
                 System.out.println(difficultyBox.getSelectedItem());
             }
         });
 
-        play.addActionListener(new ActionListener() {
+        play.addActionListener(new ActionListener()
+        {
+		            @Override
+		            public void actionPerformed(ActionEvent e)
+		            {
+		                SudokuGUI.startTimer(0, 0);
+		                frame.dispose();
+		                new SudokuGUI();
+		                System.out.println("Score play listener: " + String.valueOf(SudokuGUI.score));
+		                /*SudokuGUI.myCardLayout mcl = sg.new myCardLayout();
+		                mcl.createAndShowGUI();*/
+		            }
+        });
+
+        manual.addActionListener(new ActionListener()
+        {
             @Override
-            public void actionPerformed(ActionEvent e) {
-                SudokuGUI.startTimer(0, 0);
-                frame.dispose();
-                new SudokuGUI();
-                System.out.println("Score play listener: " + String.valueOf(SudokuGUI.score));
-                /*SudokuGUI.myCardLayout mcl = sg.new myCardLayout();
-                mcl.createAndShowGUI();*/
+            public void actionPerformed(ActionEvent e)
+            {
+				{
+					mFrame = new JFrame();
+					mTextArea = new JTextArea();
+					mPanel = new JPanel();
+					back = new JButton("Close");
+
+					try
+					{
+						fr = new FileReader(manualFile);
+						reader = new BufferedReader(fr);
+						mTextArea.read(reader, "mTextArea");
+						reader.close();
+						mTextArea.requestFocus();
+					}
+					catch (IOException ex)
+					{
+						System.out.println("Your manual is missing.");
+					}
+
+					mPanel.add(mTextArea);
+					mPanel.add(back);
+					back.addActionListener(new ActionListener()
+					{
+								@Override
+								public void actionPerformed(ActionEvent e)
+								{
+									mFrame.dispose();
+								}
+					});
+
+					mFrame.add(mPanel);
+					mFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+					mFrame.setVisible(true);
+					mFrame.setSize(500, 500);
+				}
             }
         });
+
 
 
         highscore.setBackground(new Color(126, 237, 235));
