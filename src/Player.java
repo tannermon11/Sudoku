d1a665d7b9f6ae3ae46bf2badce14bb9429454a3
surfusa <surfusa@users.noreply.github.com>
2014-11-18 11:28:51 -0700

    Update Player.java
    Creating proper methods that get values and creates new files.

--- src/Player.java
+++ src/Player.java
@@ -1,14 +1,13 @@
 import java.io.BufferedReader;
 import java.io.File;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.DocumentBuilder;
-import org.w3c.dom.Document;
-import org.w3c.dom.NodeList;
-import org.w3c.dom.Node;
-import org.w3c.dom.Element;
-
-import java.io.FileReader;
+import javax.xml.parsers.*;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.*;
+import javax.xml.transform.stream.*;
+import org.xml.sax.*;
+import org.w3c.dom.*;
 import java.util.*;
+
 /**
  * User: Tanner
  * Date: 11/4/2014
@@ -17,35 +16,101 @@ import java.util.*;
  */
 public class Player
 {
-	String username, password, secretQuestion;
-	/*try
+	String username = null;
+	String password = null;
+	String secretQuestion = null;
+	String secretAnswer = null;
+	ArrayList<String> ListofPlayers;
+
+	public boolean createPlayer(String xml)
 	{
-		File userFolder = new File("/UserInformation");
-		FileReader fr = null;
-		BufferedReader reader = null;
-		File[] listOfProfiles = userFolder.listFiles();
+		List = new ArrayList<String>();
+		Document document;
+		DocumentBuilder
 		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-		DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-		Document[] Profiles = dBuilder.parse(listOfProfiles);
-
-	}
-	ArrayList<String> listOfNames = new ArrayList<String>();
-
-	for(int i = 0; i < listOfProfiles.length; i++)
-	{
-		if(listOfProfiles[i].isFile())
+		try
 		{
-			listOfName.add(listOfProfiles[i].getName());
+			DocumentBuilder db = dbFactory.newDoumentBuilder();
+
+			document = db.parse(xml)
+			Element doc = document.getDocumentElement();
+
+			username = getTextValue(username, doc, "username");
+			if (username != null)
+			{
+				if(!username.isEmpty())
+					{username.add(username);}
+			}
 		}
 	}
 
-	String getName(File User+".xml")
+	public void saveUser(String xmlFile)
 	{
-		return eElement.getElementsBy
+		Document document;
+		Element element = null;
+
+		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+		try
+		{
+			DocumentBuilder db = dbFactory.newDoumentBuilder();
+			document = db.newDocument();
+
+			Element root = document.createElement("User Information");
+
+			element = document.createElement("Username");
+			element.appendChild(dom.createElement(username));
+			root.appendChild(element);
+
+
+			document.appendChild(root);
+			try
+			{
+				Transformer tr = TransformerFactory.newInstance().newTransformer();
+				tr.setOutputProperty(OutputKeys.INDENT, "yes");
+				tr.setOutputProperty(OutputKeys.METHOD, "xml");
+				tr.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+				tr.setOutputProperty(OutputKeys.DOCTYPE_SYSTEM, "roles.dtd");
+				tr.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+
+				tr.transform(new DOMSource(document)), new StreamResults(new FileOutputStream(xmlFile)));
+			}
+			catch(TransformerException te)
+			{
+				System.out.println(te.getMessage());
+			}
+			catch(IOException ioe)
+			{
+				System.out.println(ioe.getMessage());
+			}
+		}
+		catch (ParserConfigurationException pce)
+		{
+			System.out.println("UsersXML: Error trying to instantiate DocumentBuilder " + pce);
+    	}
 	}
 
-    void CreateUser(String name, String code, String question)
-    {
-		if(
-	}*/
+	public void setTextValue(String information, Element doc, String tag)
+	{
+		Node node = getNode(tag,
+
+		NodeList nodeList = node.getElementsByTagName(tag);
+		for (int i = 0; i < childNodes.getLength(); i++)
+		{
+			Node info = child
+		}
+
+	}
+
+	public String getTextValue(String information, Element doc, String tag)
+	{
+		String info = information;
+		NodeList nodeList = doc.getElementsByTagName(tag);
+
+		if (nodeList.getLength() > 0 && nodeList.item(0).hasChildNodes())
+		{
+			info = nodeList.item(0).getFirstChild().getNodeValues();
+		}
+
+		return info;
+	}
 }
